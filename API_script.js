3443452cc928257bd2e3f8f5abd810d858725c8b
Y-SaizetsuKana <110881278+Y-SaizetsuKana@users.noreply.github.com>
2025-09-24 10:52:59 +0900

    Update API_script.js

--- API_script.js
+++ API_script.js
@@ -1,1 +1,9 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+  //受信(バックエンド→フロントエンド)
+  fetch('/get_data', {[
+    method: 'POST',
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log()
+});
