ad9a80a0db35704eae9bb155b9c1b70c84bc48ab
Y-SaizetsuKana <110881278+Y-SaizetsuKana@users.noreply.github.com>
2025-09-24 10:47:29 +0900

    Update API_app.py

--- API_app.py
+++ API_app.py
@@ -1,1 +1,25 @@
+from flask import Flask, render_template, jsonify, request
 
+app = Flask(__name__)
+#送信(バックエンド→フロントエンド)
+@app.route('/')
+def index():
+  return render_templete('index.html')
+
+@app.route('/get_data', methods=['POST'])
+def get_data():
+  data_from_python = {'message': 'Output Data'}
+  return jsonify(data_from_python)
+
+if __name__ == '__main__':
+  app.run(debug = True)
+  
+#受信(フロントエンド→バックエンド)
+@app.route('/receive_data', methods=['POST'])
+def receive_data():
+  data_from_js = request.json
+  print('Front End', data_from_js)
+  return jsonify({'message': 'Input Data'})
+
+if __name__ == '__main__':
+  app.run(debug = True)
