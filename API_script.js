a215bfa71625b394f1090df5691506837f5897f3
Y-SaizetsuKana <110881278+Y-SaizetsuKana@users.noreply.github.com>
2025-09-24 11:02:24 +0900

    Update API_script.js

--- API_script.js
+++ API_script.js
@@ -5,5 +5,26 @@ document.addEventListener('DOMContentLoaded', function() {
   })
   .then(response => response.json())
   .then(data => {
-    console.log()
+    console.log('Back End', data);
+  })
+  .catch(error => {
+    console.error('Error', error)
+  });
+
+  //送信(フロントエンド→バックエンド)
+  const dataToPython = {messege: 'Input Data'};
+  fetch('/receive_data', {
+    method: 'POST',
+    headers: [
+      'Content-Type': 'application/json',
+      },
+       body: JSON.stringify(dataToPython),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Back End:', data));
+  })
+  .catch(error => {
+    consoke.error('Error', error);
+  });
 });
